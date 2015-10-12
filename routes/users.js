94e015b8c739562a7def471be7fc67b518608185
Komaa <pietro.rancatik@gmail.com>
2015-10-12 19:16:38 +0300

    setted enviroment and created user

--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,12 @@ router.route('/users').get(function(req, res) {
     if (err) {
       return res.send(err);
     }
-    console.log(users);
     res.json(users);
   });
 });
 
 router.route('/users').post(function(req, res) {
   var user = new User(req.body);
-  console.log(user);
-  console.log(req.body);
   user.save(function(err) {
     if (err) {
       return res.send(err);
