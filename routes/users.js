fe66f66c61f6bb635e8b60954c269765afabbcc1
Komaa <pietro.rancatik@gmail.com>
2015-10-13 11:38:00 +0300

    finished user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var User = require('../models/user');
 var express = require('express');
 var router = express.Router();
 
+//get all the users
 router.route('/users').get(function(req, res) {
   User.find(function(err, users) {
     if (err) {
@@ -11,6 +12,7 @@ router.route('/users').get(function(req, res) {
   });
 });
 
+//insert a new user
 router.route('/users').post(function(req, res) {
   var user = new User(req.body);
   user.save(function(err) {
@@ -21,4 +23,25 @@ router.route('/users').post(function(req, res) {
   });
 });
 
+//modify an user
+router.route('/users/:id').put(function(req,res){
+  User.findOne({ _id: req.params.id }, function(err, user) {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (prop in req.body) {
+      user[prop] = req.body[prop];
+    }
+
+    // save the user
+    user.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json({ message: 'User updated!' });
+    });
+  });
+});
 module.exports = router;
