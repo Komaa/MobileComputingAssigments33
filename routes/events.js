6edf3c1a978d605ec385b9ac136c762108a58d0c
Komaa <pietro.rancatik@gmail.com>
2015-10-13 18:21:12 +0300

    added model events and basic functions

--- routes/events.js
+++ routes/events.js
@@ -71,8 +71,8 @@ router.route('/events/:id').put(function(req,res){
 
 //retriving a event by id
 router.route('/events/search/:id_user').get(function(req, res) {
-  console.log(req.params.id);
-  Event.findOne({ _id: req.params.id, user_id:req.params.id_user}, function(err, event) {
+  console.log(req.query.id);
+  Event.findOne({ _id: req.query.id, user_id:req.params.id_user}, function(err, event) {
     if (err) {
       return res.send(err);
     }
