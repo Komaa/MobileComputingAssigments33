152ecdb786237759c8e93946f47acd8125133dfc
Komaa <pietro.rancatik@gmail.com>
2015-10-13 16:16:35 +0300

    user autentication

--- routes/users.js
+++ routes/users.js
@@ -69,4 +69,15 @@ router.route('/users/:id').delete(function(req, res) {
   });
 });
 
+//retriving a user
+router.route('/users/login').get(function(req, res) {
+  User.findOne({ username: req.params.username, password; req.params.password}, function(err, user) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.json(user);
+  });
+});
+
 module.exports = router;
