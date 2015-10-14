e25feab45e4d877e9dcfa3cf88b05aaf04896e6e
Komaa <pietro.rancatik@gmail.com>
2015-10-14 16:25:55 +0300

    adding geolocation query

--- routes/events.js
+++ routes/events.js
@@ -117,6 +117,8 @@ router.route('/events/search/bylocation/:id_user').get(function(req, res) {
   coords[0] = req.query.longitude;
   coords[1] = req.query.latitude;
   req.query.distance /= 6371;
+  console.log(coords);
+  console.log(req.query.distance);
   Event.find({ location: {$near: coords, $maxDistance: req.query.distance}, id_user:req.params.id_user}, function(err, event) {
     if (err) {
       return res.send(err);
