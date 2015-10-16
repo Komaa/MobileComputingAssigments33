cdf427f78679d71ed1147c453a18b55f2de7ff40
Komaa <pietro.rancatik@gmail.com>
2015-10-16 15:55:52 +0300

    adding invitation mail

--- routes/events.js
+++ routes/events.js
@@ -133,8 +133,11 @@ router.route('/events/search/bylocation/:id_user').get(function(req, res) {
 //send an event by mail
 router.route('/events/invite/:id_user').post(function(req, res) {
   var email=req.body.mail;
-  req.query.id=req.body.id;
-  var event=router.route('/events/search/:id_user').get(function(req, res);
+  var event=Event.findOne({ _id:req.body.id_event, id_user:req.params.id_user}, function(err, event) {
+    if (err) {
+      return res.send(err);
+    }
+  });
   console.log(event.name);
   var user=req.params.id_user;
 
