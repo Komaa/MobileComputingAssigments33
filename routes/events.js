039e49c2d29bb75b493446c181b474eaac16813a
Komaa <pietro.rancatik@gmail.com>
2015-10-16 18:49:55 +0300

    function finished

--- routes/events.js
+++ routes/events.js
@@ -44,14 +44,11 @@ router.route('/events/:id').post(function(req, res) {
 //copy an event
 router.route('/events/copyevent/:id').post(function(req, res) {
   _ = require('underscore');
-  Event.findOne({ _id:req.body.id_event}, '-__v', function(err, event) {
+  Event.findOne({ _id:req.body.id_event}, function(err, event) {
     if (err) {
       return res.send(err);
     }
     event.id_user=req.params.id;
-    event._id=null;
-    event.increment();
-    delete event._id;
     console.log(event);
 
     event.save(function(err) {
