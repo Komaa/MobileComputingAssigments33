51cb36fd085fb2c8608a2571d91a72352ae8a7f1
Komaa <pietro.rancatik@gmail.com>
2015-10-16 16:33:42 +0300

    adding invitation mail

--- routes/events.js
+++ routes/events.js
@@ -154,10 +154,10 @@ router.route('/events/invite/:id_user').post(function(req, res) {
           });
 
         var text = 'Greeting from Strax Calendar team! \n\n'+
-        'The user '+ user.username + 'invited you to the event ' + event.name + '/n/n'+
-        +'Please click on the following link to accept the invitation:/n/n'+
+        'The user '+ user.username + ' invited you to the event ' + event.name +
+        +'\n\nPlease click on the following link to accept the invitation:\n\n'+
         '130.233.42.94:8080/api/events/acceptinvitation/'+ event._id+
-        +'/n/nBest Regards';
+        +'\n\nBest Regards';
 
           var mailOptions = {
             from: 'mobilecalendar33@gmail.com', // sender address
